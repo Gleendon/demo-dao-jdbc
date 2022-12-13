ba16a8f0a6c6d3a98f4ede866cfee70f32eefe98
Glendon Souza <102700138+Gleendon@users.noreply.github.com>
2022-12-13 10:58:46 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,20 @@
 package application;
 
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Department dp = new Department(6, "boobks");
+		
+		Seller seller = new Seller(20, "Valleria", "Valleria@gmail", new Date(), 5000.0, dp);
+		
+		
+		System.out.println(seller);
 	}
 
 }
