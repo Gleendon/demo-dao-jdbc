076c084726d68a8a858439c25ffa022714187602
Glendon Souza <102700138+Gleendon@users.noreply.github.com>
2022-12-14 19:01:31 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -19,11 +18,19 @@ public class Program {
 		
 		System.out.println(seller);
 		
-		System.out.println("\n Procurando vendedor pelo id do departamentp");
+		System.out.println("\n Procurando vendedor pelo id do departamento");
 		Department dep = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartament(dep);
 		
 		for(Seller seller2 : list) {
+			
+			System.out.println(seller2);
+		}
+	
+		System.out.println("\n Buscando todos os vendedores");
+		List<Seller> list3 = sellerDao.findAll();
+		
+		for(Seller seller2 : list3) {
 			
 			System.out.println(seller2);
 		}
